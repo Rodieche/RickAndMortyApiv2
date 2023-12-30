11c4fab043c0c52032d55b92ec1a2fd98015aca8
Rodolfo Echenique <rechenique@MacBook-Pro-de-Rodolfo.local>
2023-12-30 22:53:19 +1300

    Feat: Se añade el titulo a Characters

--- src/pages/Characters.jsx
+++ src/pages/Characters.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react"
 import { getData } from "../helpers/api-request";
 import { Card } from "../components/Card";
+import { Typography } from "keep-react";
 
 export const Characters = () => {
 
@@ -20,14 +21,14 @@ export const Characters = () => {
 
     return (
     <>
+        <Typography className="text-center m-10">Rick and Morty Characters</Typography>
         <div className="flex items-center justify-center">
-            <div className="grid grid-cols-1 md:grid-cols-4" style={{ marginTop: '100px' }}>
+            <div className="grid grid-cols-1 md:grid-cols-4">
                 {characters.map(character => {
                     return <Card character={character}  key={ character.id } />
                 })}
             </div>
         </div>
-
     </>
   )
 }
